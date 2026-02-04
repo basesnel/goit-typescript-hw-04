2ba54ef2da52fdf9d633b565a5bb7485adeeafbd
basesnel <basesnel@gmail.com>
2026-02-04 15:57:00 +0200

    Project is initiated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
-import { Route, Routes } from "react-router-dom";
-import SharedLayout from "./components/SharedLayout/SharedLayout";
-import Welcome from "./pages/Welcome/Welcome";
-import Main from "./pages/Main/Main";
-import NotFound from "./pages/NotFound/NotFound";
-import Entities from "./pages/Entities/Entities";
-
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Welcome />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/entities" element={<Entities />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
-  );
+  return <h1>App</h1>;
 };
 
 export default App;
--- src/entities/index.js
+++
@@ -1,5 +0,0 @@
-import { System } from "./system";
-import { Malware } from "./malware";
-import { Network } from "./network";
-
-export { System, Malware, Network };
