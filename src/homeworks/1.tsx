1fd6a9c614431c0af0ad28166ba93ced2767f0ee
basesnel <basesnel@gmail.com>
2026-02-25 10:16:31 +0200

    Added notification to Task01.

--- src/homeworks/1.tsx
+++ src/homeworks/1.tsx
@@ -1,22 +1,37 @@
+import { useState } from "react";
 import { Item, ItemFlex, ItemText, List, Observer } from "@components";
 
+import styles from "./styles.module.css";
+
 const Task01 = () => {
+  const [note, useNote] = useState(false);
+
+  const { notification } = styles;
+
   return (
-    <Observer
-      onContentEndVisible={() => {
-        console.log("End is reached.");
-      }}
-    >
-      <List message="There is no items">
-        {[...Array(30)].map((_, i) => (
-          <Item key={i}>
-            <ItemFlex>
-              <ItemText>{`Item ${i + 1}`}</ItemText>
-            </ItemFlex>
-          </Item>
-        ))}
-      </List>
-    </Observer>
+    <>
+      {note && (
+        <div className={notification} onClick={() => useNote(false)}>
+          <span>End of list is reached</span>
+        </div>
+      )}
+      <Observer
+        onContentEndVisible={() => {
+          console.log("End is reached.");
+          useNote(true);
+        }}
+      >
+        <List message="There is no items">
+          {[...Array(30)].map((_, i) => (
+            <Item key={i}>
+              <ItemFlex>
+                <ItemText>{`Item ${i + 1}`}</ItemText>
+              </ItemFlex>
+            </Item>
+          ))}
+        </List>
+      </Observer>
+    </>
   );
 };
 
