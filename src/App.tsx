955f3263323722007e088885e21fb6a6fa40de82
basesnel <basesnel@gmail.com>
2026-03-02 14:57:55 +0200

    Task02 is solved.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Card, Container, Grid, H, Main } from "@components";
 import { Task01v2 } from "./homeworks/Task01v2/Task01v2";
 import { Task01v1 } from "./homeworks/Task01v1/Task01v1";
+import Task02 from "./homeworks/Task02/Task02";
 
 const App = () => {
   return (
@@ -16,21 +17,9 @@ const App = () => {
           <Card title={<H level={2}>task 1 version 2</H>}>
             <Task01v2 />
           </Card>
-          {[...Array(4)].map((_, i) => (
-            <div
-              key={i}
-              style={{
-                width: "100%",
-                height: "200px",
-                background: "yellow",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              {i + 1}
-            </div>
-          ))}
+          <Card title={<H level={2}>task 2</H>}>
+            <Task02 />
+          </Card>
         </Grid>
       </Container>
     </Main>
---
+++ src/homeworks/Task02/Task02.tsx
@@ -0,0 +1,79 @@
+import { useReducer } from "react";
+
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: "start" | "pending" | "finished" | "idle";
+};
+
+type Action = {
+  type:
+    | "START_REQUEST"
+    | "PENDING_REQUEST"
+    | "FINISH_REQUEST"
+    | "RESET_REQUEST";
+};
+
+const initialState: State = {
+  isRequestInProgress: false,
+  requestStep: "idle",
+};
+
+let timerID: ReturnType<typeof setTimeout> | undefined;
+
+function requestReducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "START_REQUEST":
+      return { ...state, isRequestInProgress: true, requestStep: "start" };
+    case "PENDING_REQUEST":
+      return { ...state, isRequestInProgress: true, requestStep: "pending" };
+    case "FINISH_REQUEST":
+      return { ...state, isRequestInProgress: false, requestStep: "finished" };
+    case "RESET_REQUEST":
+      return { ...state, isRequestInProgress: false, requestStep: "idle" };
+    default:
+      return state;
+  }
+}
+
+export function Task02() {
+  const [requestState, requestDispatch] = useReducer(
+    requestReducer,
+    initialState,
+  );
+
+  const startRequest = (): void => {
+    requestDispatch({ type: "START_REQUEST" });
+    // Імітуємо запит до сервера
+    timerID = setTimeout(() => {
+      requestDispatch({ type: "PENDING_REQUEST" });
+      // Імітуємо отримання відповіді від сервера
+      setTimeout(() => {
+        requestDispatch({ type: "FINISH_REQUEST" });
+        clearTimeout(timerID);
+        timerID = undefined;
+      }, 2000);
+    }, 2000);
+  };
+
+  const resetRequest = (): void => {
+    if (requestState.requestStep === "start") {
+      clearTimeout(timerID);
+      requestDispatch({ type: "RESET_REQUEST" });
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={startRequest}>Почати запит</button>
+      <button
+        onClick={resetRequest}
+        disabled={!(requestState.requestStep === "start")}
+      >
+        Скинути запит
+      </button>
+      <p>Стан запиту: {requestState.requestStep}</p>
+    </div>
+  );
+}
+
+export default Task02;
