c6b3732d713e083a6286ea41d44c28282deb81d3
basesnel <basesnel@gmail.com>
2026-03-05 14:49:10 +0200

    Button props are corrected by TypeScript Interface

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,38 +1,17 @@
 import styles from "./styles.module.css";
 
-type ButtonProps = {
-  children: React.ReactNode | string;
-  id?: string | undefined;
-  type?: "button" | "submit" | "reset";
-  onClick?: () => void;
-  disabled?: boolean;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   full?: boolean;
-  delegated?: any;
-};
+}
 
 const Button = (props: ButtonProps) => {
-  const {
-    children,
-    id,
-    type = "button",
-    onClick,
-    disabled,
-    full,
-    ...delegated
-  } = props;
+  const { children, full, ...rest } = props;
   const { button, fullfilled } = styles;
 
   const btnStyle = full ? fullfilled : button;
 
   return (
-    <button
-      id={id}
-      className={btnStyle}
-      type={type}
-      onClick={onClick}
-      disabled={disabled}
-      {...delegated}
-    >
+    <button className={btnStyle} {...rest}>
       {children}
     </button>
   );
