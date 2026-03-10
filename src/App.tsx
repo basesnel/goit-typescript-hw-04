798300a4e51348391b8eda9c3f0b028b319dac47
basesnel <basesnel@gmail.com>
2026-03-10 14:53:46 +0200

    Task04 is resolved.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Task01v2 } from "./homeworks/Task01v2/Task01v2";
 import { Task01v1 } from "./homeworks/Task01v1/Task01v1";
 import Task02 from "./homeworks/Task02/Task02";
 import Task03 from "./homeworks/Task03/Task03";
+import Task04 from "./homeworks/Task04/Task04";
 
 const App = () => {
   return (
@@ -23,6 +24,9 @@ const App = () => {
           </Card>
           <Card title={<H level={2}>task 3</H>}>
             <Task03 />
+          </Card>
+          <Card title={<H level={2}>task 4</H>}>
+            <Task04 />
           </Card>
         </Grid>
       </Container>
