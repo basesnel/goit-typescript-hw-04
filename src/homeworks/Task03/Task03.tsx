405714098e5b597a07b1490033e70d072ed402d2
basesnel <basesnel@gmail.com>
2026-03-11 15:19:34 +0200

    Text status is added to Task03.

--- src/homeworks/Task03/Task03.tsx
+++ src/homeworks/Task03/Task03.tsx
@@ -4,19 +4,34 @@ import { Input } from "@components";
 
 const Task03 = () => {
   const [value, setValue] = useState<string>("");
+  const [notify, setNotify] = useState<string>("");
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
+    setNotify(getNotify(event.target.value));
+  };
+
+  // const handleInput = (event: InputEvent<HTMLInputElement>) => {
+  //   setValue(event.target.value);
+  // };
+
+  const getNotify = (value: string): string => {
+    const text = "The field change event typed correctly";
+
+    return text.substring(0, value.length);
   };
 
   return (
-    <Input
-      caption="Input field"
-      type="text"
-      value={value}
-      placeholder="Type something..."
-      onChange={handleChange}
-    />
+    <>
+      <Input
+        caption="Input field"
+        type="text"
+        value={value}
+        placeholder="Type something..."
+        onChange={handleChange}
+      />
+      <p>{notify}</p>
+    </>
   );
 };
 
