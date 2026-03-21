e2497da6a453321cead601fdd10718707a2f85b2
basesnel <basesnel@gmail.com>
2026-03-21 18:20:28 +0200

    Types are some optimized.

--- src/types/components.ts
+++ src/types/components.ts
@@ -3,63 +3,45 @@ import type { ButtonHTMLAttributes } from "react";
 import type { InputHTMLAttributes } from "react";
 import type { LiHTMLAttributes } from "react";
 
-type ChildrenProps = {
-  children: ReactNode;
-};
+type ChildrenProps = { children: ReactNode };
 
-type CardTitle = {
-  title: ReactNode;
+type CardTitle = { title: ReactNode };
+type FlexBox = {
+  direction?: "row-reverse" | "column" | "column-reverse";
+  align?: "stretch" | "flex-start" | "flex-end" | "baseline";
+  wrapped?: "wrap" | "nowrap" | "wrap-reverse";
 };
+type HLevel = { level: number };
+type ItemFlex = { spaceBetween?: boolean };
+type ObserverV1 = { onContentEndVisible: () => void };
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  full?: boolean;
-}
+type CardProps = CardTitle & ChildrenProps;
+type FlexBoxProps = FlexBox & ChildrenProps;
+type Hprops = HLevel & ChildrenProps;
+type ItemFlexProps = ItemFlex & ChildrenProps;
+type ObserverV1Props = ObserverV1 & ChildrenProps;
 
 type CaptionProps = {
   label: string;
   note: string;
 };
 
-type CardProps = CardTitle & ChildrenProps;
+type ItemTextProps = { children: string | number };
 
-type FlexBox = {
-  direction?: "row-reverse" | "column" | "column-reverse";
-  align?: "stretch" | "flex-start" | "flex-end" | "baseline";
-  wrapped?: "wrap" | "nowrap" | "wrap-reverse";
+type ListProps = {
+  message: string;
+  children: ReactNode[] | null;
 };
 
-type FlexBoxProps = FlexBox & ChildrenProps;
-
-type HLevel = {
-  level: number;
-};
-
-type Hprops = HLevel & ChildrenProps;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  full?: boolean;
+}
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
 }
 
 interface ItemProps extends LiHTMLAttributes<HTMLLIElement> {}
-
-type ItemFlexProps = {
-  children: ReactNode;
-  spaceBetween?: boolean;
-};
-
-type ItemTextProps = {
-  children: string | number;
-};
-
-type ListProps = {
-  message: string;
-  children: React.ReactNode[] | null;
-};
-
-type ObserverV1Props = {
-  children: ReactNode;
-  onContentEndVisible: () => void;
-};
 
 export type {
   ButtonProps,
